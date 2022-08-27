da64798bb1f02608b5d49fd202deadb09a9aa24c
D. B. Moore <sttyerase@gmail.com>
2022-08-26 22:19:50 -0400

    Reformat code properly using IDE.

--- src/main/java/com/dbmi/demos/magdexux/Article.java
+++ src/main/java/com/dbmi/demos/magdexux/Article.java
@@ -1,88 +1,90 @@
 package com.dbmi.demos.magdexux;
 
 public class Article {
-        protected long     articleId;
-        protected String articleTitle;
-        protected String articleAuthor;
-        protected String articleSynopsis;
-        protected String  articleCategory;
-        protected String articleKeywords;
-        protected int    articleMonth;
-        protected int    articleYear;
+    protected long articleId;
+    protected String articleTitle;
+    protected String articleAuthor;
+    protected String articleSynopsis;
+    protected String articleCategory;
+    protected String articleKeywords;
+    protected int articleMonth;
+    protected int articleYear;
 
-        public Article() {}
+    public Article() {
+    }
 
-        public Article(long id, String title, String synopsis, String category, String keywords,
-                       int month, int year) {
-            this.articleId          = id;
-            this.articleTitle       = title;
-            this.articleSynopsis    = synopsis;
-            this.articleCategory    = category;
-            this.articleKeywords     = keywords;
-            this.articleMonth       = month;
-            this.articleYear        = year;
-        } // CONSTRUCTOR(INT,STRING,STRING)
+    public Article(long id, String title, String synopsis, String category, String keywords,
+                   int month, int year) {
+        this.articleId = id;
+        this.articleTitle = title;
+        this.articleSynopsis = synopsis;
+        this.articleCategory = category;
+        this.articleKeywords = keywords;
+        this.articleMonth = month;
+        this.articleYear = year;
+    } // CONSTRUCTOR(INT,STRING,STRING)
 
-        public long getArticleId() {
-            return articleId;
-        } // GETARTICLEID()
+    public long getArticleId() {
+        return articleId;
+    } // GETARTICLEID()
 
-        public void setArticleId(long articleId) {
-            this.articleId = articleId;
-        }
+    public void setArticleId(long articleId) {
+        this.articleId = articleId;
+    }
 
-        public String getArticleTitle() {
-            return articleTitle;
-        }
+    public String getArticleTitle() {
+        return articleTitle;
+    }
 
-        public void setArticleTitle(String articleTitle) {
-            this.articleTitle = articleTitle;
-        }
+    public void setArticleTitle(String articleTitle) {
+        this.articleTitle = articleTitle;
+    }
 
-        public String getArticleSynopsis() {
-            return articleSynopsis;
-        }
+    public String getArticleSynopsis() {
+        return articleSynopsis;
+    }
 
-        public void setArticleSynopsis(String articleSynopsis) {
-            this.articleSynopsis = articleSynopsis;
-        }
+    public void setArticleSynopsis(String articleSynopsis) {
+        this.articleSynopsis = articleSynopsis;
+    }
 
-        public String getArticleCategory() {
-            return articleCategory;
-        }
+    public String getArticleCategory() {
+        return articleCategory;
+    }
 
-        public void setArticleCategory(String articleCategory) {
-            this.articleCategory = articleCategory;
-        }
+    public void setArticleCategory(String articleCategory) {
+        this.articleCategory = articleCategory;
+    }
 
-        public String getArticleKeywords() {
-            return articleKeywords;
-        }
+    public String getArticleKeywords() {
+        return articleKeywords;
+    }
 
-        public void setArticleKeywords(String articleKeywords) {
-            this.articleKeywords = articleKeywords;
-        }
+    public void setArticleKeywords(String articleKeywords) {
+        this.articleKeywords = articleKeywords;
+    }
 
-        public int getArticleMonth() {
-            return articleMonth;
-        }
+    public int getArticleMonth() {
+        return articleMonth;
+    }
 
-        public void setArticleMonth(int articleMonth) {
-            this.articleMonth = articleMonth;
-        }
+    public void setArticleMonth(int articleMonth) {
+        this.articleMonth = articleMonth;
+    }
 
-        public int getArticleYear() {
-            return articleYear;
-        }
+    public int getArticleYear() {
+        return articleYear;
+    }
 
-        public void setArticleYear(int articleYear) {
-            this.articleYear = articleYear;
-        }
-        public String getArticleAuthor() {
-            return articleAuthor;
-        }
+    public void setArticleYear(int articleYear) {
+        this.articleYear = articleYear;
+    }
 
-        public void setArticleAuthor(String articleAuthor) {
-            this.articleAuthor = articleAuthor;
-        }
+    public String getArticleAuthor() {
+        return articleAuthor;
+    }
+
+    public void setArticleAuthor(String articleAuthor) {
+        this.articleAuthor = articleAuthor;
+    }
 } // CLASS
